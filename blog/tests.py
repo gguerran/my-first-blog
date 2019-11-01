a2fb168582fef86d0b49710a851eae50cfacac60
gguerran <gustavoguerra.gr@gmail.com>
2019-11-01 08:18:36 -0300

    Tentativa de testes 01

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,31 @@
-# from django.test import TestCase
-
+from django.test import TestCase
+from django.urls import reverse
 # Create your tests here.
+
+
+class HomeTest(TestCase):
+    def setUp(self):
+
+        self.resp = self.client.get('/')
+
+    def test_get(self):
+        """Deve retornar código de estado 200"""
+        self.assertEqual(self.resp.status_code, 200)
+
+    def test_template(self):
+        """Verificando se a template usada é a blog/post_list.html"""
+        self.assertTemplateUsed(self.resp, 'blog/post_list.html')
+
+
+class PostListAuthorTest(TestCase):
+    def setUp(self):
+        url = reverse('post_list_by_author', kwargs={'author_': 'admin'})
+        self.resp = self.client.get(url)
+
+    def test_get(self):
+        """Deve retornar código de estado 200"""
+        self.assertEqual(self.resp.status_code, 200)
+
+    def test_template(self):
+        """Verificando se a template usada é a blog/post_list.html"""
+        self.assertTemplateUsed(self.resp, 'blog/post_list.html')
