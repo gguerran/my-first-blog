3f0c8cb4e7b2850e67e5914b5801907cc4a0c789
gguerran <gustavoguerra.gr@gmail.com>
2019-11-01 10:50:54 -0300

    Teste 01

--- blog/tests.py
+++ blog/tests.py
@@ -1,12 +1,14 @@
 from django.test import TestCase
 from django.urls import reverse
+from .forms import PostForm
+from .models import Post
+from django.contrib.auth.models import User
 # Create your tests here.
 
 
 class HomeTest(TestCase):
     def setUp(self):
-        url = reverse('post_list')
-        self.resp = self.client.get(url)
+        self.resp = self.client.get(reverse('post_list'))
 
     def test_get(self):
         """Deve retornar código de estado 200"""
@@ -19,8 +21,12 @@ class HomeTest(TestCase):
 
 class PostListAuthorTest(TestCase):
     def setUp(self):
-        url = reverse('post_list_by_author', kwargs={'author_': 'admin'})
-        self.resp = self.client.get(url)
+        user = User(email='nickb@wnj.com', username='test')
+        user.set_password('N%sd00_pTs')
+        user.save()
+        self.resp = self.client.get(
+            reverse('post_list_by_author', kwargs={'author_': user.username})
+        )
 
     def test_get(self):
         """Deve retornar código de estado 200"""
@@ -34,14 +40,39 @@ class PostListAuthorTest(TestCase):
 
 class PostDetailTest(TestCase):
     def setUp(self):
-        url = reverse('post_detail', kwargs={'pk': 3})
-        self.resp = self.client.get(url)
+        user = User(email='nickb@wnj.com', username='test')
+        user.set_password('N%sd00_pTs')
+        user.save()
+        post = Post(
+            title='Teste01',
+            subtitle='Testando 01',
+            text='texto texto',
+            author=user)
+        post.save()
+        self.resp = self.client.get(
+            reverse('post_detail', kwargs={'pk': post.pk}))
 
     def test_get(self):
         """Deve retornar código de estado 200"""
         self.assertEqual(self.resp.status_code, 200)
-    '''
+
     def test_template(self):
         """Verificando se a template usada é a blog/post_list.html"""
-        self.assertTemplateUsed(self.resp, 'blog/post_list.html')
-    '''
+        self.assertTemplateUsed(self.resp, 'blog/post_detail.html')
+
+
+class PostAddTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/post/new/')
+
+    def test_get(self):
+        """Deve retornar código de estado 200"""
+        self.assertEqual(self.resp.status_code, 200)
+
+    def test_template(self):
+        """Verificando se a template usada é a blog/post_list.html"""
+        self.assertTemplateUsed(self.resp, 'blog/post_edit.html')
+
+    def test_add_form(self):
+        form = self.resp.context['form']
+        self.assertIsInstance(form, PostForm)
